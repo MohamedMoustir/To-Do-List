a3b3a03f169e454cb03f8e7d478fba37ecfc924a
MohamedMoustir <itsmoustir@gmail.com>
2024-10-30 13:06:52 +0100

    edit function ajoute

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 let setdata;
-
+var tmp = null;
+let mood= 'create'
+console.log(tmp);
 
 // get data in html
 const addTasks = document.getElementById("addTasks");
 //add tasks
 addTasks.addEventListener('click', function () {
+ 
+
   if (document.getElementById("description").value == '' || document.getElementById("titer").value == '' || document.getElementById("date_limite").value == '') {
     alert("email is found")
 
@@ -34,15 +38,26 @@ addTasks.addEventListener('click', function () {
       description: description.value,
 
     }
-
-    setdata.push(getDta)
-    localStorage.setItem("task", JSON.stringify(setdata))
+    
+    console.log("id is ",tmp); 
+    
+     if(mood =='create'){
+      setdata.push(getDta)
+      
+     }else{
+      setdata[tmp]=getDta;
+      tmp =null
+     }
+    
+     localStorage.setItem("task", JSON.stringify(setdata))
 
     setDataInnerHtml()
     closeCardsTasks()
     clearFormFields()
+    location.reload()
   }
 })
+console.log(mood);
 
 
 // clear data
@@ -73,98 +88,51 @@ function setDataInnerHtml() {
 
 
   setdata.forEach(task => {
-    console.log(task.Status);
+
+    taskHTML.innerHTML += `
+        <div  >
+          <div class="bg-white shadow-md border border-red-600 border-l-8 p-4 mb-4 w-64 relative" >
+            <div class="absolute leading-3 font-bold right-2 top-0 size-5">
+              <h1 onclick="openChoxtodo()">...</h1>
+            </div>
+            <div class="relative max-w-sm">
+              <div class="absolute z-10 flex items-center ps-1 pointer-events-none right-11 top-8">
+                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                  <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+                </svg>
+              </div>
+              <h1 id="default-datepicker"  class="border border-red-700   w-16 absolute right-0 top-7 px-5">${task.date_limite}</h1>
+            </div>
+            <h3 class="font-bold">${task.Titer}</h3>
+            <p class="text-zinc-500 text-xs my-2">${task.description}</p>
+            <div class="flex justify-between mt-3">
+              <button type="button" onclick="removeTask(${task.id})" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-2">Remove</button>
+              <button type="button"  onclick="UpditeTask(${task.id})" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-3">Update</button>
+            </div>
+          </div>
+          </div>
+        `;
+
     switch (task.Status) {
-
       case "To Do":
-        taskContainer1.innerHTML += `
-          <div  >
-            <div class="bg-white shadow-md border border-red-600 border-l-8 p-4 mb-4 w-64 relative" >
-              <div class="absolute leading-3 font-bold right-2 top-0 size-5">
-                <h1 onclick="openChoxtodo()">...</h1>
-              </div>
-              <div class="relative max-w-sm">
-                <div class="absolute z-10 flex items-center ps-1 pointer-events-none right-11 top-8">
-                  <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-                  </svg>
-                </div>
-                <h1 id="default-datepicker"  class="border border-red-700   w-16 absolute right-0 top-7 px-5">${task.date_limite}</h1>
-              </div>
-              <h3 class="font-bold">${task.Titer}</h3>
-              <p class="text-zinc-500 text-xs my-2">${task.description}</p>
-              <div class="flex justify-between mt-3">
-                <button type="button" onclick="openpopUpremove(${task.id})" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-2">Remove</button>
-                <button type="button"  onclick="Update(${task.id})" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-3">Update</button>
-              </div>
-            </div>
-            </div>
-          `;
+        taskContainer1.innerHTML += taskHTML;
         break;
-
-
-      case "In Progess":
-
-        taskContainer2.innerHTML += `
-          <div class="">
-          <div class="bg-white shadow-md border border-green-600 border-l-8 p-4 mb-4 w-64  block justify-center" >
-            <div class="absolute leading-3 font-bold right-2 top-0 size-5">
-              <h1 onclick="openChoxtodo()">...</h1>
-            </div>
-            <div class="relative max-w-sm">
-              <div class="absolute z-10 flex items-center ps-1 pointer-events-none right-11 top-8">
-                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                  <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-                </svg>
-              </div>
-              <h1 id="default-datepicker"  class="border border-red-700   w-16 absolute right-0 top-7 px-5">${task.date_limite}</h1>
-            </div>
-            <h3 class="font-bold">${task.Titer}</h3>
-            <p class="text-zinc-500 text-xs my-2">${task.description}</p>
-            <div class="flex justify-between mt-3">
-              <button type="button" onclick="openpopUpremove(${task.id})" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-2">Remove</button>
-              <button type="button"  onclick="Update(${task.id})" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-3">Update</button>
-            </div>
-          </div>
-          </div>
-        `;
-
-
-break;
-  
+      case "In Progress":
+        taskContainer2.innerHTML += taskHTML;
+        break;
       case "Done":
-
-        taskContainer3.innerHTML += `
-          <div class="">
-          <div class="bg-white shadow-md border border-black-600 border-l-8 p-4 mb-4 w-64  block justify-center" >
-            <div class="absolute leading-3 font-bold right-2 top-0 size-5">
-              <h1 onclick="openChoxtodo()">...</h1>
-            </div>
-            <div class="relative max-w-sm">
-              <div class="absolute z-10 flex items-center ps-1 pointer-events-none right-11 top-8">
-                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                  <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-                </svg>
-              </div>
-              <h1 id="default-datepicker"  class="border border-red-700   w-16 absolute right-0 top-7 px-5">${task.date_limite}</h1>
-            </div>
-            <h3 class="font-bold">${task.Titer}</h3>
-            <p class="text-zinc-500 text-xs my-2">${task.description}</p>
-            <div class="flex justify-between mt-3">
-              <button type="button" onclick="openpopUpremove(${task.id})" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-2">Remove</button>
-              <button type="button"  onclick="Update(${task.id})" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-3">Update</button>
-            </div>
-          </div>
-          </div>
-        `;
+        taskContainer3.innerHTML += taskHTML;
+        break;
       default:
         break;
     }
-
+  
   });
 
 
 }
+
+
 
 
 
@@ -192,14 +160,17 @@ function OpenMnue() {
 
 // function Open cards Add Tasks and add style in header
 let crud_modal = document.getElementById("crud-modal")
+
 function openCardsTasks() {
   crud_modal.classList.toggle("hidden");
   header.classList.toggle('blur-sm');
+
 }
-//  close 
+//  close cards add tasks
 function closeCardsTasks() {
   crud_modal.classList.toggle("hidden");
   header.classList.toggle('blur-sm');
+  
 }
 
 
@@ -207,22 +178,41 @@ function closeCardsTasks() {
 
 
 // function popup Remove
-function openpopUpremove(id) {
+function removeTask(id) {
   // let popup_remove= document.getElementById("popup-remove");
   //  popup_remove.classList.toggle("hidden");
   //  header.classList.toggle('blur-sm');
-  setdata = setdata.filter(task => task.id !== id);
+  setdata = setdata.filter(filter => filter.id !== id);
   localStorage.setItem("task", JSON.stringify(setdata));
   setDataInnerHtml();
-
 }
 
 function closepopupremove() {
   let popup_remove = document.getElementById("popup-remove");
   popup_remove.classList.toggle("hidden");
   header.classList.toggle('blur-sm');
-
 }
 
 
+// function Update Tasks
 
+
+function UpditeTask(i) {
+  crud_modal.classList.toggle("hidden");
+  header.classList.toggle('blur-sm');
+  addTasks.innerHTML="Updite";
+
+
+const task = setdata.find(task => task.id === i);
+
+if (task) {
+  document.getElementById("description").value = task.description;
+  document.getElementById("titer").value = task.Titer;
+  document.getElementById("priorite").value = task.priorite;
+  document.getElementById("Status").value = task.Status;
+  document.getElementById("date_limite").value = task.date_limite;
+}
+ mood = 'updite';
+tmp=i;
+
+ }
