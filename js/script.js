1be058b4ed751074ecee18de24edab8bf191a370
MohamedMoustir <itsmoustir@gmail.com>
2024-10-30 17:30:14 +0100

    Commence les function Recherch

--- js/script.js
+++ js/script.js
@@ -28,9 +28,9 @@ addTasks.addEventListener('click', function () {
     } else {
       setdata = [];
     }
-
+   
     let getDta = {
-      id: Date.now(),
+      id:Math.random() * 100,
       Titer: Titer.value,
       priorite: priorite.value,
       Status: Status.value,
@@ -88,13 +88,45 @@ function setDataInnerHtml() {
 
 
   setdata.forEach(task => {
+  
+    switch (task.Status) {
 
-    taskHTML.innerHTML += `
-        <div  >
-          <div class="bg-white shadow-md border border-red-600 border-l-8 p-4 mb-4 w-64 relative" >
-            <div class="absolute leading-3 font-bold right-2 top-0 size-5">
-              <h1 onclick="openChoxtodo()">...</h1>
+      case "To Do":
+        
+        taskContainer1.innerHTML += `
+          <div  >
+            <div class="bg-white shadow-md border border-red-600 border-l-8 p-4 mb-4 w-64 relative" >
+              <div class="absolute leading-3 font-bold right-2 top-0 size-5">
+                <h1 onclick="openChoxtodo()">...</h1>
+              </div>
+              <div class="relative max-w-sm">
+                <div class="absolute z-10 flex items-center ps-1 pointer-events-none right-11 top-8">
+                  <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                    <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+                  </svg>
+                </div>
+                <h1 id="default-datepicker"  class="border border-red-700   w-16 absolute right-0 top-7 px-5">${task.date_limite}</h1>
+              </div>
+              <h3 class="font-bold">${task.Titer}</h3>
+              <p class="text-zinc-500 text-xs my-2">${task.description} | <span>${task.priorite}</p>
+              <div class="flex justify-between mt-3">
+                <button type="button" onclick="removeTask(${task.id})" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-2">Remove</button>
+                <button type="button"  onclick="UpditeTask(${task.id})" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-3">Update</button>
+              </div>
             </div>
+            </div>
+          `;
+        break;
+
+        
+      case "In Progess":
+       
+        taskContainer2.innerHTML += `
+          <div class="">
+          <div class="bg-white shadow-md border border-black border-l-8 p-4 mb-4 w-64  block justify-center" >
+             <div class="absolute leading-3 font-bold right-2 top-0 size-5">
+                <h1 onclick="openChoxtodo()">...</h1>
+              </div>
             <div class="relative max-w-sm">
               <div class="absolute z-10 flex items-center ps-1 pointer-events-none right-11 top-8">
                 <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
@@ -104,7 +136,7 @@ function setDataInnerHtml() {
               <h1 id="default-datepicker"  class="border border-red-700   w-16 absolute right-0 top-7 px-5">${task.date_limite}</h1>
             </div>
             <h3 class="font-bold">${task.Titer}</h3>
-            <p class="text-zinc-500 text-xs my-2">${task.description}</p>
+            <p class="text-zinc-500 text-xs my-2">${task.description} | <span>${task.priorite}</p>
             <div class="flex justify-between mt-3">
               <button type="button" onclick="removeTask(${task.id})" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-2">Remove</button>
               <button type="button"  onclick="UpditeTask(${task.id})" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-3">Update</button>
@@ -113,18 +145,38 @@ function setDataInnerHtml() {
           </div>
         `;
 
-    switch (task.Status) {
-      case "To Do":
-        taskContainer1.innerHTML += taskHTML;
-        break;
-      case "In Progress":
-        taskContainer2.innerHTML += taskHTML;
-        break;
+        
+break;
+
       case "Done":
-        taskContainer3.innerHTML += taskHTML;
-        break;
+        
+        taskContainer3.innerHTML += `
+           <div class="">
+          <div class="bg-white shadow-md border border-green-600 border-l-8 p-4 mb-4 w-64  block justify-center" >
+             <div class="absolute leading-3 font-bold right-2 top-0 size-5">
+                <h1 onclick="openChoxtodo()">...</h1>
+              </div>
+            <div class="relative max-w-sm">
+              <div class="absolute z-10 flex items-center ps-1 pointer-events-none right-11 top-8">
+                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                  <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+                </svg>
+              </div>
+              <h1 id="default-datepicker"  class="border border-red-700   w-16 absolute right-0 top-7 px-5">${task.date_limite}</h1>
+            </div>
+            <h3 class="font-bold">${task.Titer}</h3>
+            <p class="text-zinc-500 text-xs my-2">${task.description} | <span>${task.priorite}</span></p>
+            <div class="flex justify-between mt-3">
+              <button type="button" onclick="removeTask(${task.id})" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-2">Remove</button>
+              <button type="button"  onclick="UpditeTask(${task.id})" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-3">Update</button>
+            </div>
+          </div>
+          </div>
+        `;
+        
       default:
         break;
+
     }
   
   });
@@ -215,4 +267,13 @@ if (task) {
  mood = 'updite';
 tmp=i;
 
+ }
+ 
+ function recherche(value){
+ let recher = document.getElementById("recher")
+  setdata = setdata.filter(filter => filter.Titer === value);
+  setDataInnerHtml();
+  if (recher.value == '') {
+    location.reload()
+  }
  }
