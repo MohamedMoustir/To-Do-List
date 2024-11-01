c57001c53cc7672b1b0c4599a4c0077e137d7e56
MohamedMoustir <itsmoustir@gmail.com>
2024-11-01 15:01:47 +0100

    added function color and style ajoute

--- js/script.js
+++ js/script.js
@@ -1,27 +1,26 @@
 
-let setdata;
+
 var tmp = null;
+// let setdata = JSON.parse(localStorage.getItem("task")) || [];
+
+let description = document.getElementById("description");
+let Titer = document.getElementById("titer");
+let priorite = document.getElementById("priorite");
+let Status = document.getElementById("Status");
+let date_limite = document.getElementById("date_limite");
+let local = JSON.parse(localStorage.getItem("task"))||[];
+let id;
+
+let taskContainer1 = document.getElementById("task1");
+let taskContainer2 = document.getElementById("task2");
+let taskContainer3 = document.getElementById("task3");
+
+
 
 function addTask(){
   if (document.getElementById("description").value == '' || document.getElementById("titer").value == '' || document.getElementById("date_limite").value == '') {
     alert("email is found")
-
-
   } else {
-
-    let description = document.getElementById("description");
-    let Titer = document.getElementById("titer");
-    let priorite = document.getElementById("priorite");
-    let Status = document.getElementById("Status");
-    let date_limite = document.getElementById("date_limite");
-    let id;
-
-
-    if (localStorage.getItem("task") != null) {
-      setdata = JSON.parse(localStorage.getItem("task"))
-    } else {
-      setdata = [];
-    }
 
     let getDta = {
       id: Math.random() * 100,
@@ -42,18 +41,15 @@ function addTask(){
     }
 
     localStorage.setItem("task", JSON.stringify(setdata))
+    
 
     setDataInnerHtml()
     closeCardsTasks()
     clearFormFields()
     location.reload()
     sortTasksByPriority();
-    // triPar_priorite();
+
   }
-  
-
-
-
 }
 
 
@@ -70,148 +66,104 @@ function clearFormFields() {
 }
 
 // ajout data in html
-
 function setDataInnerHtml() {
-  // declartion task
-  let taskContainer1 = document.getElementById("task1");
-  let taskContainer2 = document.getElementById("task2");
-  let taskContainer3 = document.getElementById("task3");
-
-  //declartion count
-  let countone = document.getElementById("countone");
-  let counttwe = document.getElementById("counttwe");
-  let counttree = document.getElementById("counttree");
-
-    let countToDo = 0;
-    let countDoing = 0;
-    let countDone = 0;
-    
-
   taskContainer1.innerHTML = "";
   taskContainer2.innerHTML = "";
   taskContainer3.innerHTML = "";
 
-  setdata.forEach(task => {
-    
-    switch (task.Status) {
-    
-      case "To Do":
-       
-        taskContainer1.innerHTML += `
-          <div>
-            <div class="bg-white shadow-md border  border-l-8 p-4 mb-4 w-64 relative card">
-              <div class="absolute leading-3 font-bold right-2 top-0 size-5">
-                <h1 onclick="openChoxtodo()">...</h1>
-              </div>
-              <div class="relative max-w-sm">
-                <div class="absolute z-10 flex items-center ps-1 pointer-events-none right-11 top-8">
-                  <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-                  </svg>
-                </div>
-                <h1 id="default-datepicker"  class="border border-red-700   w-16 absolute right-0 top-7 px-5">${task.date_limite}</h1>
-              </div>
-              <h3 class="font-bold">${task.Titer}</h3>
-              <p class="text-zinc-500 text-xs my-2">${task.description} | <span>${task.priorite}</p>
-              <div class="flex justify-between mt-3">
-                <button type="button" onclick="removeTask(${task.id})" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-2">Remove</button>
-                <button type="button"  onclick="UpditeTask(${task.id})" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-3">Update</button>
-              </div>
-            </div>
-            </div>
-          `;
-
-          countToDo++;
-          
-        break;
-
-
-
-      case "In Progess":
-       
-        taskContainer2.innerHTML += `
-          <div class="">
-          <div class="bg-white shadow-md border border-yellow-300 border-l-8 p-4 mb-4 w-64  block justify-center" >
-             <div class="absolute leading-3 font-bold right-2 top-0 size-5">
-                <h1 onclick="openChoxtodo()">...</h1>
-              </div>
-            <div class="relative max-w-sm">
-              <div class="absolute z-10 flex items-center ps-1 pointer-events-none right-11 top-8">
-                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                  <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-                </svg>
-              </div>
-              <h1 id="default-datepicker"  class="border border-red-700   w-16 absolute right-0 top-7 px-5">${task.date_limite}</h1>
-            </div>
-            <h3 class="font-bold">${task.Titer}</h3>
-            <p class="text-zinc-500 text-xs my-2">${task.description} | <span>${task.priorite}</p>
-            <div class="flex justify-between mt-3">
-              <button type="button" onclick="removeTask(${task.id})" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-2">Remove</button>
-              <button type="button"  onclick="UpditeTask(${task.id})" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-3">Update</button>
-            </div>
-          </div>
-          </div>
-        `;
-        countDoing++;
-
-        break;
-
-      case "Done":
-
-        taskContainer3.innerHTML += `
-           <div class="">
-          <div class="bg-white shadow-md border border-green-600 border-l-8 p-4 mb-4 w-64  block justify-center" >
-             <div class="absolute leading-3 font-bold right-2 top-0 size-5">
-                <h1 onclick="openChoxtodo()">...</h1>
-              </div>
-            <div class="relative max-w-sm">
-              <div class="absolute z-10 flex items-center ps-1 pointer-events-none right-11 top-8">
-                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                  <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-                </svg>
-              </div>
-              <h1 id="default-datepicker"  class="border border-red-700   w-16 absolute right-0 top-7 px-5">${task.date_limite}</h1>
-            </div>
-            <h3 class="font-bold">${task.Titer}</h3>
-            <p class="text-zinc-500 text-xs my-2">${task.description} | <span>${task.priorite}</span></p>
-            <div class="flex justify-between mt-3">
-              <button type="button" onclick="removeTask(${task.id})" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-2 py-2">Remove</button>
-              <button type="button"  onclick="UpditeTask(${task.id})" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-3">Update</button>
-            </div>
-          </div>
-          </div>
-        `;
-
-        countDone++;
-      default:
-        break;
-       
-    }
-    // count
-    countone.innerText=countToDo;
-    counttwe.innerText=countDoing;
-    counttree.innerText=countDone;
-
-
-  });
-
-  // countone.style.color="red"
+  let countToDo = 0;
+  let countDoing = 0;
+  let countDone = 0;
 
   
-  //  switch (priorite.value) {
-  //   case "P1":
-  //     document.getElementById("card").color.border="2px solid red";
-      
+  setdata.forEach(task => {
+    let borderColor
+    if (task.priorite === "P1") {
+      borderColor = "4px solid red";
+    } else if (task.priorite === "P2") {
+      borderColor = "4px solid yellow";
+    } else if (task.priorite === "P3") {
+      borderColor = "4px solid green";
+    }
 
+    // Add task to the correct container based on status
+    if (task.priorite == "P1") {
 
-  //     break;
-  //  }
+        
+        if (task.Status == "To Do") {
+          taskContainer1.innerHTML += ajoute(task,borderColor) 
+         
+        } 
+        if (task.Status == "In Progess") {
+          taskContainer2.innerHTML += ajoute(task,borderColor) 
+          
+         } 
+         if (task.Status == "Done") {
+          taskContainer3.innerHTML += ajoute(task,borderColor) 
+          
+         } 
+      countToDo++;
+    } else if (task.priorite === "P2") {
+      if (task.Status == "To Do") {
+        taskContainer1.innerHTML += ajoute(task,borderColor) 
+       
+      } 
+      if (task.Status == "In Progess") {
+        taskContainer2.innerHTML += ajoute(task,borderColor) 
+        
+       } 
+       if (task.Status == "Done") {
+        taskContainer3.innerHTML += ajoute(task,borderColor) 
+        
+       } 
+      countDoing++;
+    } else if (task.priorite === "P3") {
+      if (task.Status == "To Do") {
+        taskContainer1.innerHTML += ajoute(task,borderColor) 
+       
+      } 
+      if (task.Status == "In Progess") {
+        taskContainer2.innerHTML += ajoute(task,borderColor) 
+        
+       } 
+       if (task.Status == "Done") {
+        taskContainer3.innerHTML += ajoute(task,borderColor) 
+        
+       } 
+      countDone++;
+    }
+  });
 
+  // Update task counts
+  document.getElementById("countone").innerText = countToDo;
+  document.getElementById("counttwe").innerText = countDoing;
+  document.getElementById("counttree").innerText = countDone;
 }
 
-
-
-
+function ajoute(item,borderColor) {
+  let taskHTML= `
+      <div class="bg-white shadow-md border border-l-8 p-4 mb-4 w-64 relative card" style="border-left:${borderColor}">
+        <div class="absolute leading-3 font-bold right-2 top-0 size-5">
+          <h1 onclick="openChoxtodo()">...</h1>
+        </div>
+        <div class="relative max-w-sm">
+          <div class="absolute z-10 flex items-center ps-1 pointer-events-none right-11 top-8">
+            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20">
+              <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+            </svg>
+          </div>
+          <h1 class="border border-red-700 w-16 absolute right-0 top-7 px-5">${item.date_limite}</h1>
+        </div>
+        <h3 class="font-bold">${item.Titer}</h3>
+        <p class="text-zinc-500 text-xs my-2">${item.description} | <span>${item.priorite}</span></p>
+        <div class="flex justify-between mt-3">
+          <button onclick="removeTask(${item.id})" class="text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-2 py-2">Remove</button>
+          <button onclick="UpditeTask(${item.id})" class="text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-3">Update</button>
+        </div>
+      </div>
+    `;
+    return taskHTML
+}
 
 
 // onload data
@@ -222,8 +174,6 @@ window.onload = function () {
   setDataInnerHtml();
 
 };
-
-
 
 
 
@@ -297,40 +247,14 @@ function UpditeTask(i) {
 
 function recherche(value) {
   let recher = document.getElementById("recher")
-
-  setdata = setdata.filter(filter => filter.Titer == recher.value);
-  
+  setdata = setdata.filter(filter => filter.Titer == recher.value);  
     setDataInnerHtml();
 
-  // 
-  if (recher.value == '') {
-    location.reload()
-  }
 }
 
-// function triPar_priorite(){
-  
-//   taskContainer1.innerHTML = "";
-//   taskContainer2.innerHTML = "";
-//   taskContainer3.innerHTML = "";
 
-// setdata.forEach(p => {
-//   if(p.priorite =='P1'){
-//     taskContainer1.innerHTML += setDataInnerHtml(p);
-//     console.log(p.priorite);
-//   }
- 
-  
-// });
-// // setdata.forEach(f => {
-// //   if(f.priorite=='P2'){
-// //     taskContainer2.innerHTML += setDataInnerHtml(f)
-// //   }
-// // });
-// // setdata.forEach(e => {
-// //   if(e.priorite=='P3'){
-    
-// //     taskContainer2.innerHTML +=setDataInnerHtml(e); 
-// //   }
-// // });
+
 // }
+
+// triPar_priorite()
+// دالة عرض المهام في 
