55a2f4f2d0b783203a86ad10eb7a846225660f92
MohamedMoustir <itsmoustir@gmail.com>
2024-11-02 00:40:43 +0100

    add function tri

--- js/script.js
+++ js/script.js
@@ -9,25 +9,31 @@ let Titer = document.getElementById("titer");
 let priorite = document.getElementById("priorite");
 let Status = document.getElementById("Status");
 let date_limite = document.getElementById("date_limite");
-let local = JSON.parse(localStorage.getItem("task"))||[];
+let local = JSON.parse(localStorage.getItem("task")) || [];
 let id;
-
+let triparP = document.getElementById("p1")
 let taskContainer1 = document.getElementById("task1");
 let taskContainer2 = document.getElementById("task2");
 let taskContainer3 = document.getElementById("task3");
+let setdata = [];
+let triparTiter =document.getElementById("triparTiter")
 
 
-function addTask(){
+// console.log(local);
+
+function addTask() {
+
   let validTiter = /^[a-zA-Z]{1,6}\s?/ig;
   let validDescription = /^[a-zA-Z0-9]{1,10}\s?/ig;
-  if(!validTiter.test(Titer.value) ){
-alert("ccc")
+  if (!validTiter.test(Titer.value)) {
+    alert("ccc")
 
-  }else if (!validDescription.test(description.value)){
-    
+  } else if (!validDescription.test(description.value)) {
+
     alert("hhhhhhhhhhhhhhhhhhhhhh")
-    
-   } else{  
+
+  } else {
+
     let getDta = {
       id: Math.random() * 100,
       Titer: Titer.value,
@@ -37,26 +43,33 @@ alert("ccc")
       description: description.value,
 
     }
-// check or find id in this arry or no
+    // check or find id in this arry or no
     const index = setdata.findIndex(task => task.id === tmp);
     if (index !== -1) {
       setdata[index] = getDta;
     } else {
-      setdata.push(getDta); 
-      
+      setdata.push(getDta);
+
     }
 
+
+
+
+
+
+
     localStorage.setItem("task", JSON.stringify(setdata))
-    
+
     setDataInnerHtml()
     closeCardsTasks()
     clearFormFields()
     location.reload()
-    sortTasksByPriority();
+    // sortTasksByPriority();
+    // triage()
   }
-  }
-  
- 
+
+
+}
 
 
 
@@ -81,98 +94,106 @@ function setDataInnerHtml() {
   let countDoing = 0;
   let countDone = 0;
 
-  
+
   setdata.forEach(task => {
+
     let borderColor
-    if (task.priorite === "P1") {
+    if (task.priorite === "1") {
       borderColor = "4px solid red";
-    } else if (task.priorite === "P2") {
+    } else if (task.priorite === "2") {
       borderColor = "4px solid yellow";
-    } else if (task.priorite === "P3") {
+    } else if (task.priorite === "3") {
       borderColor = "4px solid green";
     }
 
     // Add task to the correct container based on status
-    if (task.priorite == "P1") {
+    if (task.priorite == "1") {
 
-        
-        if (task.Status == "To Do") {
-          taskContainer1.innerHTML += ajoute(task,borderColor) 
-         
-        } 
-        if (task.Status == "In Progess") {
-          taskContainer2.innerHTML += ajoute(task,borderColor) 
-          
-         } 
-         if (task.Status == "Done") {
-          taskContainer3.innerHTML += ajoute(task,borderColor) 
-          
-         } 
+      if (task.Status == "To Do") {
+
+        taskContainer1.innerHTML += ajoute(task, borderColor)
+
+      }
+      if (task.Status == "In Progess") {
+
+        taskContainer2.innerHTML += ajoute(task, borderColor)
+
+      }
+      if (task.Status == "Done") {
+
+        taskContainer3.innerHTML += ajoute(task, borderColor)
+
+      }
       countToDo++;
-    } else if (task.priorite === "P2") {
+    } else if (task.priorite === "2") {
       if (task.Status == "To Do") {
-        taskContainer1.innerHTML += ajoute(task,borderColor) 
-       
-      } 
+        taskContainer1.innerHTML += ajoute(task, borderColor)
+
+      }
       if (task.Status == "In Progess") {
-        taskContainer2.innerHTML += ajoute(task,borderColor) 
-        
-       } 
-       if (task.Status == "Done") {
-        taskContainer3.innerHTML += ajoute(task,borderColor) 
-        
-       } 
+        taskContainer2.innerHTML += ajoute(task, borderColor)
+
+      }
+      if (task.Status == "Done") {
+        taskContainer3.innerHTML += ajoute(task, borderColor)
+
+      }
       countDoing++;
-    } else if (task.priorite === "P3") {
+    } else if (task.priorite === "3") {
       if (task.Status == "To Do") {
-        taskContainer1.innerHTML += ajoute(task,borderColor) 
-       
-      } 
+        taskContainer1.innerHTML += ajoute(task, borderColor)
+
+      }
       if (task.Status == "In Progess") {
-        taskContainer2.innerHTML += ajoute(task,borderColor) 
-        
-       } 
-       if (task.Status == "Done") {
-        taskContainer3.innerHTML += ajoute(task,borderColor) 
-         countDone++;
-       } 
-     
+        taskContainer2.innerHTML += ajoute(task, borderColor)
+
+      }
+      if (task.Status == "Done") {
+        taskContainer3.innerHTML += ajoute(task, borderColor)
+        countDone++;
+      }
+
     }
-    
+
+
   });
- 
+
   // Update task counts
   document.getElementById("countone").innerText = countToDo;
   document.getElementById("counttwe").innerText = countDoing;
   document.getElementById("counttree").innerText = countDone;
 }
 
-function ajoute(item,borderColor) {
-  
-  
-  let taskHTML= `
+function ajoute(item, borderColor) {
+  const time = item.date_limite.split("-");
+  const mounth = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+  let timenwe = new Date(time);
+
+  // }
+  let taskHTML = `
       <div class="bg-white shadow-md border border-l-8 p-4 mb-4 w-64 relative card" style="border-left:${borderColor}">
         <div class="absolute leading-3 font-bold right-2 top-0 size-5">
           <h1 onclick="openChoxtodo()">...</h1>
         </div>
         <div class="relative max-w-sm">
-          <div class="absolute z-10 flex items-center ps-1 pointer-events-none right-11 top-8">
-            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20">
-              <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-            </svg>
-          </div>
-          <h1 class="border border-red-700 w-16 absolute right-0 top-7 px-5">${item.date_limite}</h1>
+  
+          <h1 class="border border-l-8 border-black w-18 absolute right-0 top-7 px-56 text-gray-700" id="limite ">${time[1]} ${mounth[Number(time[1]) - 1]}</h1>
         </div>
         <h3 class="font-bold">${item.Titer}</h3>
-        <p class="text-zinc-500 text-xs my-2">${item.description} | <span>${item.priorite}</span></p>
+        <p class="text-zinc-500 text-xs my-2">${item.description} | <span>P${item.priorite}</span></p>
         <div class="flex justify-between mt-3">
           <button onclick="removeTask(${item.id})" class="text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-2 py-2">Remove</button>
           <button onclick="UpditeTask(${item.id})" class="text-white bg-green-700 hover:bg-green-800 font-medium rounded-lg text-sm px-3">Update</button>
         </div>
       </div>
     `;
-    return taskHTML
+  return taskHTML
+
+
 }
+
+
 
 
 // onload data
@@ -213,7 +234,6 @@ function closeCardsTasks() {
 
 
 
-
 // function popup Remove
 function removeTask(id) {
   // let popup_remove= document.getElementById("popup-remove");
@@ -222,7 +242,7 @@ function removeTask(id) {
   setdata = setdata.filter(filter => filter.id !== id);
   localStorage.setItem("task", JSON.stringify(setdata));
   setDataInnerHtml();
-  
+
 }
 
 function closepopupremove() {
@@ -238,8 +258,8 @@ function UpditeTask(i) {
   crud_modal.classList.toggle("hidden");
   header.classList.toggle('blur-sm');
   addTasks.innerHTML = "Update";
- 
-  tmp = i; 
+
+  tmp = i;
 
   const task = setdata.find(task => task.id === i);
 
@@ -256,47 +276,42 @@ function UpditeTask(i) {
 
 function recherche(value) {
   let recher = document.getElementById("recher")
-  setdata = setdata.filter(filter => filter.Titer == recher.value);  
-    setDataInnerHtml();
+  setdata = setdata.filter(filter => filter.Titer == recher.value);
+  setDataInnerHtml();
 
 }
 
 
-// let  validDescription;
-
-// function name() {
-    
-//     let validTiter =/^\w+{3}/;
-//     let vh =validTiter.test(Titer.value);
-//     if(vh){
-//       alert("eee")
-//     }
-
-//   }
 
 
+triparP.onclick = function () {
+  function compare(a, b) {
+    let pA = a.priorite;
+    let pB = b.priorite;
+    return pA - pB; 
+
+  }
+  setdata = local.sort(compare);
+  setDataInnerHtml();
+}
+
+
+triparTiter.onclick = function () {
   
-  //  validDescription =/^\w+$/;
-  //  validTiter =/^\w+{3}/;
+setdata.sort((a, b) => {
+  const TiterA = a.Titer.toUpperCase();
+  const titerB = b.Titer.toUpperCase(); 
+  
+  if (TiterA < titerB) {
+    return -1;
+  }
+  if (TiterA > titerB) {
+    return 1;
+  }
+  return 0;
+});
 
-  //  
-// if (!validDescription.test(description.value)) {
-// console.log("zzzzzzzzzzzz");
-
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  setDataInnerHtml();
+  
+   
+}
